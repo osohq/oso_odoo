a0e2b61c12f0147f1cd4313eb875cd00838b9435
Alex Plotnick <alex@osohq.com>
2020-09-16 16:24:08 -0600

    make oso test use only base models

--- tests/test_oso_auth.py
+++ tests/test_oso_auth.py
@@ -1,46 +1,37 @@
-
 from odoo.tests.common import TransactionCase
 from odoo.tests import tagged
 from odoo.exceptions import AccessError
 
 from pathlib import Path
 
-
-# required so that classes are registered with oso before tests
-@tagged('-at_install', 'post_install')
+# Tags required so that classes are registered with oso before tests.
+@tagged("-at_install", "post_install")
 class TestOso(TransactionCase):
-
     def setUp(self, *args, **kwargs):
         super().setUp(*args, **kwargs)
-        oso = self.env['oso'].oso
+        oso = self.env["oso"].oso
         oso.clear()
         mypath = Path(__file__).parent
         oso.load_file(mypath / "test_policy.polar")
 
-        # failing to find user_demo because tests are running post_install
-        # demo user needed to avoid superuser case
-        user_demo = self.env.ref('base.user_demo')
+        user_demo = self.env.ref("base.user_demo")
         self.env = self.env(user=user_demo)
 
-        print("testing oso")
-
-    def _test_record_access(self):
-        message = self.env['mail.message']
-
+    def test_record_access(self):
+        qweb = self.env["ir.qweb"]
+        qweb.read()
         with self.assertRaises(AccessError):
-            message.read()
-
+            qweb.create({})
         with self.assertRaises(AccessError):
-            message.create({})
+            qweb.write({})
 
+        mail_server = self.env["ir.mail_server"].create(
+            {"name": "mail", "smtp_host": "smtp.example.org"}
+        )
         with self.assertRaises(AccessError):
-            message.write({})
-
-        model = self.env['sms.sms'].create({})
-        with self.assertRaises(AccessError):
-            model.unlink()
+            mail_server.read()
+        mail_server.unlink()
 
     def test_model_access(self):
-        # failing because uses superuser, why we need to get demo user working
         with self.assertRaises(AccessError):
-            website = self.env['ir.ui.menu'].create({"name": "menu"})
+            self.env["ir.ui.menu"].create({"name": "menu"})
--- tests/test_policy.polar
+++ tests/test_policy.polar
@@ -1,12 +1,13 @@
 allow(_actor, "create", resource) if
-    not resource matches mail::message and
-    not resource matches "ir.ui.menu";
+    not resource matches ir::qweb and # record level
+    not resource matches "ir.ui.menu"; # model level
 
 allow(_actor, "read", resource) if
-    not resource matches mail::message;
+    not (resource matches ir::mail_server and
+         resource.smtp_host = "smtp.example.org");
 
 allow(_actor, "write", resource) if
-    not resource matches mail::message;
+    not resource matches ir::qweb;
 
 allow(_actor, "unlink", resource) if
-    not resource matches sms::sms;
+    not resource matches ir::qweb;
