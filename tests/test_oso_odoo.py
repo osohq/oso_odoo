ddbcda25b143799c811963c185baf1b5c640e851
Gabe Jackson <gj@mail.co.de>
2020-12-01 16:58:45 -0500

    Hot potato

--- tests/test_oso_odoo.py
+++ tests/test_oso_odoo.py
@@ -28,6 +28,9 @@ class TestOso(TransactionCase):
     def test_partial_disjunctive_matches(self):
         self.env["oso"].oso.load_str(
             """
+                allow_model(_, _, "oso.test.foo");
+                allow(_, "create", _: oso::test::foo);
+
                 allow(_, _, bar: oso::test::bar) if check_foo(bar.foo);
                 check_foo(foo: oso::test::baz) if foo.name = "y";
                 check_foo(foo: oso::test::foo) if foo.name = "x";
@@ -39,3 +42,26 @@ class TestOso(TransactionCase):
 
         filter = self.env["oso"].authorize("a", "b", bar)
         self.assertEqual(filter, [("foo.name", "=", "x")])
+
+    def test_null_with_partial(self):
+        oso = self.env["oso"]
+        foo = self.env["oso.test.foo"]
+        oso.oso.clear_rules()
+        oso.oso.load_str(
+            """
+                allow_model(_, _, "oso.test.foo");
+                allow(_, action, _: oso::test::foo) if action in ["create", "read"];
+                allow(_, "unlink", foo: oso::test::foo) if foo.name = nil;
+            """
+        )
+
+        null_foo = foo.create({"name": None})
+        breakpoint()
+        filter = oso.authorize("a", "unlink", null_foo)
+        # TODO(gj): update the below to False after registering nil as False in Odooland.
+        self.assertEqual(filter, [("name", "=", None)])
+        null_foo.unlink()
+
+        non_null_foo = foo.create({})
+        with self.assertRaises(AccessError):
+            non_null_foo.unlink()
